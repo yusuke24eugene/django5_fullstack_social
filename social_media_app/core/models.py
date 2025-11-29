cad552a960b1f90475f66955f068f348f4e10680
PepeSmith <>
2025-11-29 21:43:00 +0800

    fixed profile_picture bug

--- social_media_app/core/models.py
+++ social_media_app/core/models.py
@@ -1,12 +1,22 @@
+import os
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# Function to generate a unique file name for profile pictures
+def user_profile_picture_path(instance, filename):
+    # Generate a unique UUID for the file name
+    ext = filename.split('.')[-1]
+    unique_filename = f'{uuid.uuid4()}.{ext}'
+    # Return the path where the file will be uploaded
+    return os.path.join('profile_pics/', str(instance.id), unique_filename)
+
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField()
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     groups = models.ManyToManyField(
